a07416423224237d97566fb46126becda107f079
William Granados <will.grand.wg@gmail.com>
2016-03-09 15:09:33 -0500

    added text box and connected frontend with backend

--- src/interface.py
+++ src/interface.py
@@ -36,6 +36,8 @@ class Window(QtGui.QMainWindow):
         self.init_UI()             
         self.init_dropdown_menu() 
         self.init_search_button()
+        self.init_editor()
+        self.layout.addStretch()
         self.show()
         
         
@@ -43,9 +45,10 @@ class Window(QtGui.QMainWindow):
         '''(Window) -> None
         Sets the dimensions and user interface
         '''
-        self.setGeometry(150, 150, 500, 300)
+        self.setGeometry(150, 150, 300, 210)
         self.setWindowTitle('PokéTbase - Pokémon Team Database')
         self.setWindowIcon(QtGui.QIcon('../res/icon/pikachu_icon.png'))
+        self.layout = QtGui.QHBoxLayout()
         
     def init_dropdown_menu(self):
         ''' (Window) -> None
@@ -56,47 +59,69 @@ class Window(QtGui.QMainWindow):
         for name in file:
             pokemon_names.append(name)
         # create 6 combo boxes since there are 6 members on a team
-        combo_box = [QtGui.QComboBox(self) for x in range(6)]
+        self._combo_box = [QtGui.QComboBox(self) for x in range(6)]
         for name in pokemon_names:
             for i in range(6):
-                combo_box[i].addItem(name)
+                self._combo_box[i].addItem(name)
                 
         # set up each user input individually
         
         # combo box 1
-        combo_box[0].move(0, 0)
-        combo_box[0].activated[str].connect(self.user_choice_one)
+        self._combo_box[0].move(0, 0)
+        self._combo_box[0].activated[str].connect(self.user_choice_one)
+        self.layout.addWidget(self._combo_box[0])
         
         # combo box 2
-        combo_box[1].move(0, 30)
-        combo_box[1].activated[str].connect(self.user_choice_two)  
+        self._combo_box[1].move(0, 30)
+        self._combo_box[1].activated[str].connect(self.user_choice_two)  
+        self.layout.addWidget(self._combo_box[1])
         
         # combo box 3
-        combo_box[2].move(0, 60)
-        combo_box[2].activated[str].connect(self.user_choice_three)  
-        
+        self._combo_box[2].move(0, 60)
+        self._combo_box[2].activated[str].connect(self.user_choice_three)  
+        self.layout.addWidget(self._combo_box[2])
+    
         # combo box 4
-        combo_box[3].move(0, 90)
-        combo_box[3].activated[str].connect(self.user_choice_four)  
-        
+        self._combo_box[3].move(0, 90)
+        self._combo_box[3].activated[str].connect(self.user_choice_four)  
+        self.layout.addWidget(self._combo_box[3])
+            
         # combo box 5
-        combo_box[4].move(0, 120)
-        combo_box[4].activated[str].connect(self.user_choice_five) 
+        self._combo_box[4].move(0, 120)
+        self._combo_box[4].activated[str].connect(self.user_choice_five) 
+        self.layout.addWidget(self._combo_box[4])
+        
         
         # combo box 6
-        combo_box[5].move(0, 150)
-        combo_box[5].activated[str].connect(self.user_choice_six)    
+        self._combo_box[5].move(0, 150)
+        self._combo_box[5].activated[str].connect(self.user_choice_six)   
+        self.layout.addWidget(self._combo_box[5])
+        
         
     def init_search_button(self):
         '''(Window) -> None
-        initalize the search button
+        Initalizes the search button
         '''
-        btn = QtGui.QPushButton('Search', self)
-        btn.clicked.connect(self.user_choice_search)
-        btn.move(0, 180)
+        self._search_btn = QtGui.QPushButton('Search', self)
+        self.layout.addWidget(self._search_btn)
+        self._search_btn.clicked.connect(self.user_choice_search)
+        self._search_btn.move(0, 180)
         
+    def init_editor(self):
+        '''(Window) -> None
+        Intializes the editor pane
+        '''
+        self._open_editor = QtGui.QTextEdit(self)
+        self._open_editor.move(100, 0)
+        self._open_editor.resize(200, 210)        
+        self.layout.addWidget(self._open_editor)
+        
+    def output_editor(self, output):
+        '''(Window, str) -> None
+        '''
+        self._open_editor.clear()
+        self._open_editor.append(output)
     
-        
     def user_choice_search(self):
         '''(Window) -> None
         check if the team is within the database then update output pane
@@ -109,36 +134,37 @@ class Window(QtGui.QMainWindow):
         if(user_input_empty == False):
             query = self._database.query(self._user_input)
             if(query != None):
-                print(query)
+                self.output_editor(str(query))
+            else:
+                self.output_editor(str(self._user_input) + '\n' + 'sorry')
         
         
     def user_choice_one(self, text):
         '''(Window) -> None'''
-        self._user_input[0] = text
+        self._user_input[0] = text.strip()
         
     def user_choice_two(self, text):
         '''(Window) -> None'''
-        self._user_input[1] = text
+        self._user_input[1] = text.strip()
     
     def user_choice_three(self, text):
         '''(Window) -> None'''
-        self._user_input[2] = text   
+        self._user_input[2] = text.strip()
         
     def user_choice_four(self, text):
         '''(Window) -> None'''
-        self._user_input[3] = text  
+        self._user_input[3] = text.strip()
         
     def user_choice_five(self, text):
         '''(Window) -> None'''
-        self._user_input[4] = text  
+        self._user_input[4] = text.strip()  
         
     def user_choice_six(self, text):
         '''(Window) -> None'''
-        self._user_input[5] = text       
+        self._user_input[5] = text.strip()       
 
 
 def main():
-
     app = QtGui.QApplication(sys.argv)
     window = Window()
     sys.exit(app.exec_())
